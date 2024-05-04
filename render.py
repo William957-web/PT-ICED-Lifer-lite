374133f16143f26e34b7c0a7165cc6599a2fd8a5
William957-web <85293841+William957-web@users.noreply.github.com>
2024-05-04 11:56:10 +0800

    Update render.py

--- render.py
+++ render.py
@@ -8,10 +8,8 @@ app = Flask(__name__)
 app.secret_key = os.urandom(16)
 Markdown(app, extensions=['tables'])
 
-# 数据库配置
 DATABASE = 'blog.db'
 
-# 创建数据库连接
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -19,14 +17,13 @@ def get_db():
         db.row_factory = sqlite3.Row
     return db
 
-# 关闭数据库连接
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-# 创建表格
+
 def init_db():
     with app.app_context():
         db = get_db()
@@ -44,8 +41,6 @@ def index():
     return render_template('index.html', comments=comments)
 
 
-
-# 用户登录
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -62,7 +57,6 @@ def login():
             return render_template('login.html', error='Invalid username or password.')
     return render_template('login.html')
 
-# 用户注销
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
@@ -104,7 +98,6 @@ def add_comment():
 def enter_pin():
     if request.method == 'POST':
         pin = request.form['pin']
-        # 檢查輸入的 PIN 碼是否符合要求
         if pin=='2694':
             session['isadmin']=True
             return redirect(url_for('success'))
